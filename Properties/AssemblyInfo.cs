09d9e45e30c08950a6ca52f0391eff284e6fb9ac
Xianglong He <admin@hxlxz.com>
2015-09-16 17:18:16 +0800

    程序信息

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("RuiRuiQQRobot")]
+[assembly: AssemblyDescription("RuiRuiQQRobot聊天机器人程序")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("何相龙")]
+[assembly: AssemblyProduct("RuiRuiQQRobot聊天机器人程序")]
+[assembly: AssemblyCopyright("GPL v3 何相龙")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//将 ComVisible 设置为 false 将使此程序集中的类型
+//对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("def88e1b-4792-439a-ad51-1d958575b90b")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: NeutralResourcesLanguage("zh-CN")]
+
