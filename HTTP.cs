13e808e130fd5ba79643558b6e887c26bd0892f9
Xianglong He <admin@hxlxz.com>
2017-02-04 16:01:46 +0800

    修改许可证为AGPL

--- HTTP.cs
+++ HTTP.cs
@@ -3,25 +3,28 @@ using System.IO;
 using System.Net;
 using System.Text;
 using System.Web;
-// *   This program is free software: you can redistribute it and/or modify
-// *   it under the terms of the GNU General Public License as published by
-// *   the Free Software Foundation, either version 3 of the License, or
-// *   (at your option) any later version.
-// *
-// *   This program is distributed in the hope that it will be useful,
-// *   but WITHOUT ANY WARRANTY; without even the implied warranty of
-// *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// *   GNU General Public License for more details.
-// *
-// *   You should have received a copy of the GNU General Public License
-// *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  <RuiRuiQQRobot: A QQ chat robot.>
+//  Copyright(C) <2015-2017>  <Xianglong He>
+
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or(at your option) any later version.
+
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU Affero General Public License for more details.
+
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.If not, see<http://www.gnu.org/licenses/>.
 // *
 // * @author     Xianglong He
-// * @copyright  Copyright (c) 2015 Xianglong He. (http://tec.hxlxz.com)
-// * @license    http://www.gnu.org/licenses/     GPL v3
-// * @version    1.0
+// * @copyright  Copyright (c) 2015-2017 Xianglong He. (http://tec.hxlxz.com)
+// * @license    http://www.gnu.org/licenses/     AGPL v3
+// * @version    0.1
 // * @discribe   RuiRuiQQRobot服务端
-// * 本软件作者是何相龙，使用GPL v3许可证进行授权。
+// * 本软件作者是何相龙，使用AGPL v3许可证进行授权。
 namespace RuiRuiQQRobot
 {
 
